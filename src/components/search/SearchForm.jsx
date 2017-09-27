61a5362ececa9070b2627cc60ae7b591a5fcc88c
Kanstantsin Kapytau <kanstantsin_kapytau@epam.com>
2017-09-27 16:48:36 +0300

    add Link to the SearchForm

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
-import ReactRouterPropTypes from 'react-router-prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function SearchForm(props) {
-  let textInput = '';
-
-  function submitHandler(e) {
-    e.preventDefault();
-    props.history.push(`/search/${textInput.value}`);
+export default class SearchForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
   }
 
-  return (
-    <form>
-      <fieldset className="search-section">
-        <label htmlFor="movie-search">Find your movie</label>
-        <input id="movie-search" type="text" ref={(input) => { textInput = input; }} />
-        <span id="arrow">⤶</span>
-      </fieldset>
-      <fieldset className="bottom-section">
-        <div className="search-filters">
-          <span>Search By</span>
-          <button className="btn btn-danger" type="button">Title</button>
-          <button className="btn" type="button">Director</button>
-        </div>
-        <div className="form-submit">
-          <button className="btn btn-danger" onClick={submitHandler} type="submit">Search</button>
-        </div>
-      </fieldset>
-    </form>
-  );
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <fieldset className="search-section">
+          <label htmlFor="movie-search">Find your movie</label>
+          <input id="movie-search" type="text" value={this.state.value} onChange={e => this.handleChange(e)} />
+          <span id="arrow">⤶</span>
+        </fieldset>
+        <fieldset className="bottom-section">
+          <div className="search-filters">
+            <span>Search By</span>
+            <button className="btn btn-danger" type="button">Title</button>
+            <button className="btn" type="button">Director</button>
+          </div>
+          <div className="form-submit">
+            <Link className="btn btn-danger" to={`/search/${this.state.value}`}>Search</Link>
+          </div>
+        </fieldset>
+      </form>
+    );
+  }
 }
-
-export default withRouter(SearchForm);
-
-SearchForm.defaultProps = {
-  history: null
-};
-
-SearchForm.propTypes = {
-  history: ReactRouterPropTypes.history.isRequired
-};
